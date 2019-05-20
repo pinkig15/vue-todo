0bb0f452b4d617dab864e1009e3b6abcd064ef2e
pinkig15 <pinkigautam15@gmail.com>
2019-05-20 12:39:22 +0530

    vue-todo-sample

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      </nav>
+        <router-view/>
+  </div>
+</template>
+
+<script>
+import Skills from "./components/Skills.vue";
+
+export default {
+  name: "app",
+  components: {
+    Skills
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+@import url("http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+body {
+  background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  justify-items: center;
+  padding-top: 50px;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+  width: 50%;
+}
+nav {
+  margin-bottom: 20px;
+}
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello {{their_name}} This is an abou page</h1>
+    <p>Lorem Ipsum</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  alert: true,
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
